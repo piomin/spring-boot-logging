8015b20ddb5eaeb4ac2469660435bafdcb18a3fa
minkowp <piotr.minkowski@play.pl>
2019-05-08 09:20:13 +0200

    adding ssl to logstash

--- src/main/java/pl/piomin/logging/config/SpringLoggingAutoConfiguration.java
+++ src/main/java/pl/piomin/logging/config/SpringLoggingAutoConfiguration.java
@@ -2,10 +2,13 @@ package pl.piomin.logging.config;
 
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.net.ssl.KeyStoreFactoryBean;
+import ch.qos.logback.core.net.ssl.SSLConfiguration;
 import net.logstash.logback.appender.LogstashTcpSocketAppender;
 import net.logstash.logback.encoder.LogstashEncoder;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -15,6 +18,7 @@ import pl.piomin.logging.util.UniqueIDGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Configuration
 public class SpringLoggingAutoConfiguration {
@@ -25,6 +29,10 @@ public class SpringLoggingAutoConfiguration {
 	String url;
 	@Value("${spring.application.name:-}")
 	String name;
+	@Value("${spring.logstash.ssl.trustStoreLocation}")
+	Optional<String> trustStoreLocation;
+	@Value("${spring.logstash.ssl.trustStorePassword}")
+	Optional<String> trustStorePassword;
 
 	@Bean
 	public UniqueIDGenerator generator() {
@@ -45,12 +53,21 @@ public class SpringLoggingAutoConfiguration {
 	}
 
 	@Bean
+	@ConditionalOnProperty("spring.logstash.enabled")
 	public LogstashTcpSocketAppender logstashAppender() {
 		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 		LogstashTcpSocketAppender logstashTcpSocketAppender = new LogstashTcpSocketAppender();
 		logstashTcpSocketAppender.setName(LOGSTASH_APPENDER_NAME);
 		logstashTcpSocketAppender.setContext(loggerContext);
 		logstashTcpSocketAppender.addDestination(url);
+		trustStoreLocation.ifPresent(it -> {
+			SSLConfiguration sslConfiguration = new SSLConfiguration();
+			KeyStoreFactoryBean factory = new KeyStoreFactoryBean();
+			factory.setLocation(it);
+			trustStorePassword.ifPresent(factory::setPassword);
+			sslConfiguration.setTrustStore(factory);
+			logstashTcpSocketAppender.setSsl(sslConfiguration);
+		});
 		LogstashEncoder encoder = new LogstashEncoder();
 		encoder.setContext(loggerContext);
 		encoder.setIncludeContext(true);
