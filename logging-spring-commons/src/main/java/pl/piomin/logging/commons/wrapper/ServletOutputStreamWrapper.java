10246a2ddfd780cb1d3475ba8611e1e9478acf67
piomin <piotr.minkowski@gmail.com>
2024-03-22 12:48:04 +0100

    2.0.3: Create commons module

---
+++ logging-spring-commons/src/main/java/pl/piomin/logging/commons/wrapper/ServletOutputStreamWrapper.java
@@ -0,0 +1,39 @@
+package pl.piomin.logging.commons.wrapper;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.WriteListener;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ServletOutputStreamWrapper extends ServletOutputStream {
+
+    private OutputStream outputStream;
+    private ByteArrayOutputStream copy;
+
+    public ServletOutputStreamWrapper(OutputStream outputStream) {
+        this.outputStream = outputStream;
+        this.copy = new ByteArrayOutputStream();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        outputStream.write(b);
+        copy.write(b);
+    }
+
+    public byte[] getCopy() {
+        return copy.toByteArray();
+    }
+
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public void setWriteListener(WriteListener writeListener) {
+
+    }
+}
